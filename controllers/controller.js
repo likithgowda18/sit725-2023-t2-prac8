77d01d0b511afdf153266348e90a0887af32dad0
Likith Gowda S <105909257+likithgowda18@users.noreply.github.com>
2023-08-30 00:52:19 +1000

    changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,33 +1,26 @@
 /* This file will handle all the database connection related stuff */
-let collection = require("../models/cat");
-const insertcatintoDB = (req, res) => {
+let collection = require('../models/cat');
+
+
+const postCat = (req,res) => {
     let cat = req.body;
-    collection.insertCat(cat, (err, result) => {
-        if (!err) {
-            res.json({
-                statusCode: 201,
-                data: result,
-                message: 'success'
-            });
-
-        }
-    });
-
-}
-
-const getcatfromDB = (req, res) => {
-    collection.getAllCats((err, result) => {
-        if (!err) {
-            res.json({
-                statusCode: 200,
-                data: result,
-                message: 'success'
-            });
+    console.log(cat);
+    collection.postCat(cat, (err,result) => {
+        if(!err){
+            res.json({statusCode:201,data:result,message:'success'});
         }
     });
 }
 
-module.exports = {
-    insertcatintoDB,
-    getcatfromDB
-};
+
+const getAllCats = (req,res) => {
+    collection.getAllCats((err,result)=>{
+        console.log(result);
+        if(!err){
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+}
+
+module.exports = {postCat, getAllCats}
+
--- routers/router.js
+++ routers/router.js
@@ -1,14 +1,14 @@
 /* This file will handle all the routing related stuff */
 let express = require('express');
 let router = express.Router();
-let controller = require("../controllers/controller");
+let controller = require('../controllers/controller');
 
-router.post('/', function(req, res) {
-    controller.insertcatintoDB(req, res);
+router.post('/', (req,res)=>{
+    controller.postCat(req,res);
 });
 
-router.get('/', (req, res) => {
-    controller.getcatfromDB(req, res);
+router.get('/', (req,res)=>{
+    controller.getAllCats(req,res);
 });
 
 module.exports = router;
